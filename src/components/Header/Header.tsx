8cc1c055a3bb5c47379f294ff16e8f771e9fd925
Mahay <hirimanana@yahoo.fr>
2026-07-10 12:05:53 +0400

    add header component to App component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,18 @@
+import { GiTakeMyMoney } from "react-icons/gi";
+
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="header-block">
+      <div className="header-col">
+        <p className="header-logo">
+          <GiTakeMyMoney />
+          <strong>SpendWise</strong>
+        </p>
+      </div>
+      <div className="header-col">
+        <a href="/profil">Profil</a>
+      </div>
+    </header>
+  );
 };
 export default Header;
