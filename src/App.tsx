8e3312649689bd87b17e2455ffab4f2856c4959e
Mahay <hirimanana@yahoo.fr>
2026-07-11 21:26:55 +0400

    config route App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.scss";
-import Header from "./components/Header/Header";
-import Menu from "./components/Menu/Menu";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Menu />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Test</div>} />
+        <Route path="/analytics" element={<div>Test</div>} />
+        <Route path="/add" element={<div>Test</div>} />
+        <Route path="/categories" element={<div>Test</div>} />
+        <Route path="/profile" element={<div>Test</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
