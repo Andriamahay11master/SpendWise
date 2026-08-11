41f3be3e80b2eb6ebaca4195c4747a9b575bc110
Mahay <hirimanana@yahoo.fr>
2026-08-11 11:54:57 +0400

    update page dashboard, add static categorydata

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,9 @@
 import DashboardCard from "./DashboardCard";
 import { FaMoneyBills } from "react-icons/fa6";
+import { IoFastFood } from "react-icons/io5";
+import { GiPartyPopper } from "react-icons/gi";
+import { MdEmojiTransportation } from "react-icons/md";
+import CategoryProgressBarCard from "../Category/CategoryProgressBarCard";
 
 const Dashboard = () => {
   const cardData = [
@@ -22,13 +26,38 @@ const Dashboard = () => {
       color: "#24d0fb",
     },
   ];
+
+  const categoryData = [
+    {
+      nameCategory: "Food",
+      iconCategory: <IoFastFood />,
+      budgetSpent: 220,
+      budgetMax: 500,
+    },
+    {
+      nameCategory: "Entertainment",
+      iconCategory: <GiPartyPopper />,
+      budgetSpent: 150,
+      budgetMax: 400,
+    },
+    {
+      nameCategory: "Transportation",
+      iconCategory: <MdEmojiTransportation />,
+      budgetSpent: 100,
+      budgetMax: 250,
+    },
+  ];
   return (
     <div className="dashboard-block">
       {cardData.map((data, index) => (
         <DashboardCard key={index} {...data} />
       ))}
       <h2 className="title-h2">Budget Overview</h2>
-      <div className="dashboard-category"></div>
+      <div className="dashboard-category">
+        {categoryData.map((data, index) => (
+          <CategoryProgressBarCard key={index} {...data} />
+        ))}
+      </div>
     </div>
   );
 };
