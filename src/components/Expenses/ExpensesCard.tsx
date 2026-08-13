95e0cbd08d42eb5bf9c9d685caad3ff299518468
Mahay <hirimanana@yahoo.fr>
2026-08-13 21:43:53 +0400

    init styling expenses card

--- src/components/Expenses/ExpensesCard.tsx
+++ src/components/Expenses/ExpensesCard.tsx
@@ -23,21 +23,19 @@ const ExpensesCard = ({
 }: ExpensesCardProps) => {
   return (
     <Link to={`/expenses/${id}`} className="expenses-card">
-      <div className="expenses-card-left">
-        <div className="expenses-card-icon">
-          {React.cloneElement(icon, { color: colorCategory })}
+      <div className="expenses-card-icon">
+        {React.cloneElement(icon, { color: colorCategory })}
+      </div>
+      <div className="expenses-card-info">
+        <div className="expenses-card-col">
+          <p className="expenses-card-description">{description}</p>
+          <p className="expenses-card-info">
+            {valueCategory}
+            <strong>{dateExpense}</strong>
+          </p>
         </div>
-        <div className="expenses-card-info">
-          <div className="expenses-card-col">
-            <p className="expenses-card-description">{description}</p>
-            <p className="expenses-card-info">
-              {valueCategory}
-              <strong>{dateExpense}</strong>
-            </p>
-          </div>
-          <div className="expenses-card-col">
-            <p className="expenses-card-amount">${montant.toFixed(2)}</p>
-          </div>
+        <div className="expenses-card-col">
+          <p className="expenses-card-amount">${montant.toFixed(2)}</p>
         </div>
       </div>
     </Link>
