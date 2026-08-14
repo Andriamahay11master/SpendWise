384ba2980c583239eea0f7e7de7df7ca3ee7a6fd
Mahay <hirimanana@yahoo.fr>
2026-08-14 20:46:04 +0400

    improve header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,34 @@
 import { GiTakeMyMoney } from "react-icons/gi";
+import { Link } from "react-router";
 
 interface HeaderProps {
   icon?: React.ReactNode;
   title?: string;
+  type?: "default" | "transaction";
 }
 const Header = ({
   icon = <GiTakeMyMoney size={30} />,
   title = "SpendWise",
+  type = "default",
 }: HeaderProps) => {
   return (
     <header className="header-block">
       <div className="header-col">
-        <p className="header-logo">
-          {icon}
-          <strong>{title}</strong>
-        </p>
+        {type === "transaction" ? (
+          <Link to="/dashboard" className="header-logo">
+            {icon}
+          </Link>
+        ) : (
+          <Link to="/" className="header-logo">
+            {icon}
+            <strong>{title}</strong>
+          </Link>
+        )}
       </div>
       <div className="header-col">
-        <a href="/profil" className="header-link">
+        <Link to="/profile" className="header-link">
           <img src="src/assets/profile.png" alt="Profile" />
-        </a>
+        </Link>
       </div>
     </header>
   );
