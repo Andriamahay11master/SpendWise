dd197c6f9c257ad463a385e251efc19e3f49c8b9
Mahay <hirimanana@yahoo.fr>
2026-08-16 12:54:34 +0400

    change structure range budget

--- src/components/Category/CategoryForm.tsx
+++ src/components/Category/CategoryForm.tsx
@@ -15,9 +15,23 @@ const CategoryForm = () => {
           <label htmlFor="selectColor">Select Color:</label>
           <input id="selectColor" type="color" placeholder="Select Color" />
         </div>
-        <div className="form-group">
-          <label htmlFor="budget">Monthly Budget Limit:</label>
-          <input id="budget" type="text" placeholder="Monthly Budget Limit" />
+        <div className="form-group form-range">
+          <div className="form-group-top">
+            <label htmlFor="budget">Monthly Budget Limit:</label>
+            <span className="currentRangeValue">$10</span>
+          </div>
+          <input
+            id="budget"
+            type="range"
+            placeholder="Monthly Budget Limit"
+            min="0"
+            max="1000"
+            step="10"
+          />
+          <div className="range-intervalle-value">
+            <p className="range-value">0</p>
+            <p className="range-value">1000</p>
+          </div>
         </div>
         <div className="form-group form-button">
           <button type="submit" className="btn btn-primary">
