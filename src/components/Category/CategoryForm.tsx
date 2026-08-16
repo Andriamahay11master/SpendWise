f6fe76ea5b026d0fa76320a717ee390400dcb8ff
Mahay <hirimanana@yahoo.fr>
2026-08-16 11:46:58 +0400

    init page add category

---
+++ src/components/Category/CategoryForm.tsx
@@ -0,0 +1,12 @@
+const CategoryForm = () => {
+  return (
+    <div className="category-form">
+      <form>
+        <input type="text" placeholder="Category Name" />
+        <button type="submit">Add Category</button>
+      </form>
+    </div>
+  );
+};
+
+export default CategoryForm;
