5aa534b67aaf9d7845adb3819d179fe0fbd31c47
Mahay <hirimanana@yahoo.fr>
2026-08-18 16:23:22 +0400

    init structure form

--- src/components/Expenses/ExpensesForm.tsx
+++ src/components/Expenses/ExpensesForm.tsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router";
+
 const ExpensesForm = () => {
   return (
-    <div className="expenses-form">
-      <h1>Expenses Form</h1>
+    <div className="main-block">
+      <div className="form-group">
+        <label htmlFor="amount">Amount</label>
+        <input type="text" name="amoun" id="amount" placeholder="0.00" />
+      </div>
+      <div className="form-group fomr-category">
+        <div className="form-group-top">
+          <label htmlFor="category">Category</label>
+          <input type="hidden" name="category" id="category" />
+          <Link to="/listCategories">View all</Link>
+        </div>
+        <div className="form-category-list"></div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="dateE">Date</label>
+        <input type="date" name="dateE" id="dateE" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="notes">Notes</label>
+        <input type="text" name="notes" id="notes" />
+      </div>
+      <div className="form-group form-submit">
+        <button className="btn btn-primary">Save Transaction</button>
+      </div>
     </div>
   );
 };
