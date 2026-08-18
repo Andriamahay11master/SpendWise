9bd4440c0c4363bce2c4c7875845358b8c49c774
Mahay <hirimanana@yahoo.fr>
2026-08-18 16:51:34 +0400

    add handleChange & useState form state management

--- src/components/Expenses/ExpensesForm.tsx
+++ src/components/Expenses/ExpensesForm.tsx
@@ -3,6 +3,7 @@ import { GiPartyPopper } from "react-icons/gi";
 import { MdEmojiTransportation } from "react-icons/md";
 import { GoArrowRight } from "react-icons/go";
 import { Link } from "react-router";
+import { useState } from "react";
 
 const ExpensesForm = () => {
   const dataCategory = [
@@ -22,17 +23,43 @@ const ExpensesForm = () => {
       color: "#f54e42",
     },
   ];
+
+  const [formData, setFormData] = useState({
+    amount: "",
+    category: "",
+    date: "",
+    notes: "",
+  });
+
+  // Single change handler for all text inputs
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
   return (
     <div className="main-block">
       <form className="form-expense">
         <div className="form-group">
           <label htmlFor="amount">Amount</label>
-          <input type="text" name="amoun" id="amount" placeholder="0.00" />
+          <input
+            type="text"
+            name="amount"
+            id="amount"
+            placeholder="0.00"
+            value={formData.amount}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group fomr-category">
           <div className="form-group-top">
             <label htmlFor="category">Category</label>
-            <input type="hidden" name="category" id="category" />
+            <input
+              type="hidden"
+              name="category"
+              id="category"
+              value={formData.category}
+              onChange={handleChange}
+            />
             <Link to="/listCategories">View all</Link>
           </div>
           <div className="form-category-list">
@@ -46,14 +73,21 @@ const ExpensesForm = () => {
         </div>
         <div className="form-group">
           <label htmlFor="dateE">Date</label>
-          <input type="date" name="dateE" id="dateE" />
+          <input type="date" name="dateE" id="dateE" value={formData.date} />
         </div>
         <div className="form-group">
           <label htmlFor="notes">Notes</label>
-          <input type="text" name="notes" id="notes" placeholder="Add notes" />
+          <input
+            type="text"
+            name="notes"
+            id="notes"
+            placeholder="Add notes"
+            value={formData.notes}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group form-submit">
-          <button className="btn btn-primary">
+          <button type="submit" className="btn btn-primary">
             <span>Save Transaction</span> <GoArrowRight />
           </button>
         </div>
