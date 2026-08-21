7d71f7a4111d9175e32265fe7049f1595554dd77
Mahay <hirimanana@yahoo.fr>
2026-08-21 12:24:31 +0400

    init part preferences page profil

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,3 +1,5 @@
+import { LuMoon } from "react-icons/lu";
+import { MdOutlineLanguage } from "react-icons/md";
 interface ProfileProps {
   image: string;
   name: string;
@@ -13,6 +15,29 @@ const Profile = ({ image, name, email }: ProfileProps) => {
         <div className="profil-info">
           <p className="profil-name">{name}</p>
           <p className="profil-email">{email}</p>
+        </div>
+      </div>
+      <div className="profil-bottom">
+        <div className="profil-setting">
+          <h3 className="title-h3">preferences</h3>
+          <div className="profil-setting-item">
+            <div className="profil-col">
+              <div className="setting-icon">
+                <LuMoon />
+              </div>
+              <p className="setting-name">Dark Mode</p>
+            </div>
+            <div className="profil-col"></div>
+          </div>
+          <div className="profil-setting-item">
+            <div className="profil-col">
+              <div className="setting-icon">
+                <MdOutlineLanguage />
+              </div>
+              <p className="setting-name">Language</p>
+            </div>
+            <div className="profil-col"></div>
+          </div>
         </div>
       </div>
     </div>
