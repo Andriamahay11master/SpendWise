b3218437d8ee531ee7bed794f900a292e374d344
Mahay <hirimanana@yahoo.fr>
2026-08-24 19:21:02 +0400

    init page analytics

--- src/components/Analytics/Analytics.tsx
+++ src/components/Analytics/Analytics.tsx
@@ -1,7 +1,18 @@
 const Analytics = () => {
+  const dataFilter = ["Week", "Month", "Year"];
   return (
-    <div className="analytics">
-      <h1>Analytics</h1>
+    <div className="main-block page-analytics">
+      <h3 className="title-h3">financial insights</h3>
+      <h2 className="title-h2">Analytics</h2>
+      <div className="filter-analytics">
+        {dataFilter.map((item, index) => {
+          return (
+            <button key={index} className="filter-analytics-item">
+              {item}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 };
