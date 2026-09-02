abd3f6cf9a3467fa33773031c6554a32e620f9a2
Mahay <hirimanana@yahoo.fr>
2026-09-02 18:45:20 +0400

    add loader for mainPage component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,19 @@
 import Header from "../components/Header/Header";
 import Menu from "../components/Menu/Menu";
+import Loader from "../components/Loader/Loader";
+import React from "react";
 
 const MainPage = ({ children }: { children: React.ReactNode }) => {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) return <Loader />;
   return (
     <div className="main-page">
       <Header />
