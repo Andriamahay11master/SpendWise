18d235b79d98dddb6f0df31643de48addee21eb7
Mahay <hirimanana@yahoo.fr>
2026-09-15 20:59:58 +0400

    add endpoint update budget

--- server/routes/budgetRoutes.ts
+++ server/routes/budgetRoutes.ts
@@ -3,6 +3,7 @@ import { Budget } from "../models/Budget";
 
 const budgetRouter = Router();
 
+// create budget
 budgetRouter.post("/api/budget", async (request, response) => {
   try {
     const { currencyCode, currency, budget } = request.body;
@@ -26,6 +27,22 @@ budgetRouter.post("/api/budget", async (request, response) => {
   }
 });
 
+//update budget
+budgetRouter.put("/api/budget", async (request, response) => {
+  try {
+    const { currencyCode, currency, budget } = request.body;
+    const budgetData = await Budget.findOneAndUpdate(
+      {},
+      { currencyCode, currency, budget, date: new Date() },
+      { new: true },
+    );
+    response.status(201).json(budgetData);
+  } catch {
+    response.status(400).json({ message: "Invalid budget data" });
+  }
+});
+
+// get current budget
 budgetRouter.get("/api/budget/current", async (_request, response) => {
   try {
     const currentBudget = await Budget.findOne().sort({ date: -1 });
