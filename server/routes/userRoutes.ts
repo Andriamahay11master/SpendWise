d7fd5c52e1e2d6a15798152eb218960654669eb8
Mahay <hirimanana@yahoo.fr>
2026-09-17 22:04:53 +0400

    add endpoint create, update, getUserByID, checkUser for user schema

---
+++ server/routes/userRoutes.ts
@@ -0,0 +1,55 @@
+import { Router } from "express";
+import { User } from "../models/User";
+
+const expenseRouter = Router();
+
+// create user
+expenseRouter.post("/api/user", async (request, response) => {
+  try {
+    const { username, email, password } = request.body;
+
+    const userData = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    response.status(201).json(userData);
+  } catch {
+    response.status(400).json({ message: "Invalid user data" });
+  }
+});
+
+// check user exist, used for login user on the app
+expenseRouter.get("/api/user/:email", async (request, response) => {
+  const email = request.params.email;
+  const user = await User.findOne({ email });
+  if (user) {
+    response.json(user);
+  } else {
+    response.status(404).json({ message: "User not found" });
+  }
+});
+
+// get user by id
+expenseRouter.get("/api/user/:id", async (request, response) => {
+  const user = await User.findById(request.params.id);
+  response.json(user);
+});
+
+// update user
+expenseRouter.put("/api/user/:id", async (request, response) => {
+  try {
+    const { username, email, password } = request.body;
+    const user = await User.findByIdAndUpdate(
+      request.params.id,
+      { username, email, password },
+      { new: true },
+    );
+    response.json(user);
+  } catch {
+    response.status(400).json({ message: "Invalid user data" });
+  }
+});
+
+export default expenseRouter;
