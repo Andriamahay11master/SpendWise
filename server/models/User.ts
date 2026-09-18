e31e25d4ff4ceb09ab2c166a6c591d1864e2208a
Mahay <hirimanana@yahoo.fr>
2026-09-18 19:31:01 +0400

    add custom function compare password to hashpassword

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -18,6 +19,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
   role: {
     type: String,
@@ -36,5 +38,20 @@ const userSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+// Middleware: Hash password before saving to the database
+userSchema.pre("save", async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified("password")) return;
+
+  // Generate a salt and hash the password (cost factor of 12 is highly secure)
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Verify plain text password against the hashed database password
+userSchema.methods.comparePassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
