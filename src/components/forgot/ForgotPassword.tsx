1103d353552e46b8506eeddb1c4516b66ee7878b
Mahay <hirimanana@yahoo.fr>
2026-09-19 18:33:19 +0400

    add handleChange, useState email, static handleSubmit

--- src/components/forgot/ForgotPassword.tsx
+++ src/components/forgot/ForgotPassword.tsx
@@ -1,10 +1,30 @@
+import { useNavigate } from "@tanstack/react-router";
+import React, { useState } from "react";
+
 const ForgotPassword = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <div className="form-page form-page-forgot">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" required />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            value={email}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group form-submit">
           <button type="button" className="btn btn-primary">
