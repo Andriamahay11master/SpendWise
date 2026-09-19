052dd1df834bae5f7a731485bb60f13355354693
Mahay <hirimanana@yahoo.fr>
2026-09-19 18:28:46 +0400

    init component

--- src/components/forgot/ForgotPassword.tsx
+++ src/components/forgot/ForgotPassword.tsx
@@ -1,5 +1,19 @@
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  return (
+    <div className="form-page form-page-forgot">
+      <form>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group form-submit">
+          <button type="button" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ForgotPassword;
